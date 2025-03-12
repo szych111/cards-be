a53ff1d0646d709c5ca6238a7609219c08bc957a
Alex <67001075+szych111@users.noreply.github.com>
2025-03-12 14:50:56 +0100

    w/o cards collections update

--- app.js
+++ app.js
@@ -43,14 +43,6 @@ mongoose
 
 const port = process.env.PORT || 8080;
 
-mongoose.connection.once("open", async () => {
-  try {
-    const db = mongoose.connection.db;
-    await db.collection("cards").rename("cards_de");
-    console.log('Collection renamed from "cards" to "cards_de"');
-  } catch (err) {
-    console.error("Error renaming collection:", err);
-  }
-
+mongoose.connection.once("open", () => {
   app.listen(port);
 });
--- routes/cards.js
+++ routes/cards.js
@@ -19,15 +19,6 @@ const countryCardModels = {
   UK: CardsUK,
 };
 
-CardsDE.updateMany({}, { $set: { project: "CloudHQ GER1", country: "DE" } })
-  .then((res) => {
-    console.log(`${res.modifiedCount} documents updated.`);
-  })
-  .catch((err) => {
-    console.error("Error updating documents:", err);
-  });
-
-
 // GET all cards for a specific country
 router.get("/:country", fbAuthUser, async (req, res, next) => {
   const country = req.params.country;
